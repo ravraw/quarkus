1fd6fda29775a91eda0e65f975f9e2ab35149202
ravraw <ravinrawat.wd@gmail.com>
2023-04-16 13:13:10 -0600

    QUARKUS:added getById method

--- src/main/java/com/crossaisles/RemoteUserProxy.java
+++ src/main/java/com/crossaisles/RemoteUserProxy.java
@@ -4,6 +4,7 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import java.util.List;
 @Path("/")
 @RegisterRestClient(baseUri = "https://jsonplaceholder.typicode.com")
@@ -11,4 +12,8 @@ public interface RemoteUserProxy {
     @GET
     @Path("users")
     List<RemoteUser> getRemoteUsers();
+
+    @GET
+    @Path("/users/{id}")
+    RemoteUser getById(@PathParam("id") String id);
 }
--- src/main/java/com/crossaisles/RemoteUserResource.java
+++ src/main/java/com/crossaisles/RemoteUserResource.java
@@ -5,6 +5,7 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
@@ -17,5 +18,12 @@ public class RemoteUserResource {
     public Response getRemoteUsers() {
         List<RemoteUser> remoteUsers = remoteUserProxy.getRemoteUsers();
         return Response.ok(remoteUsers).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getById(@PathParam("id") String id) {
+        RemoteUser remoteUser = remoteUserProxy.getById(id);
+        return Response.ok(remoteUser).build();
     }
 }
