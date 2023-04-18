a09e978bea27f7b4b4300adcdf304dc360b29667
ravraw <ravinrawat.wd@gmail.com>
2023-04-17 19:08:32 -0600

    QUARKUS: added @Timeout = 1000

--- src/main/java/com/crossaisles/RemoteUserResource.java
+++ src/main/java/com/crossaisles/RemoteUserResource.java
@@ -2,6 +2,7 @@ package com.crossaisles;
 
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.inject.Inject;
@@ -21,10 +22,15 @@ public class RemoteUserResource {
 
     @GET
     @Retry(maxRetries = 4)
+    @Timeout(1000)
     @Fallback(fallbackMethod = "getRemoteUsersFallback")
-    public Response getRemoteUsers() {
+    public Response getRemoteUsers() throws InterruptedException {
+        Long startTime = System.currentTimeMillis();
         remoteUserLogger.info("trying to fetch remote users");
         List<RemoteUser> remoteUsers = remoteUserProxy.getRemoteUsers();
+//        Thread.sleep(2000);
+        Long endTime = System.currentTimeMillis();
+        remoteUserLogger.info(String.format("GET /remote-users took %s ms to respond", (endTime - startTime)));
         return Response.ok(remoteUsers).build();
     }
 
